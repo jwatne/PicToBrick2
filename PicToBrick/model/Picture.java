a3fa0ba4e9c94538c4c69eb4b487cd787b2f0c60
jwatne <john.watne@gmail.com>
2023-09-26 05:02:48 -0500

    Checkstyle fixes.

--- PicToBrick/model/Picture.java
+++ PicToBrick/model/Picture.java
@@ -1,236 +1,278 @@
 package pictobrick.model;
 
 import javax.imageio.ImageIO;
-import java.io.*;
-import java.awt.*;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
-import java.awt.image.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.PixelGrabber;
+import java.io.File;
+import java.io.IOException;
 
 /**
- * class: Picture
- * layer: DataManagement (three tier architecture)
- * description: contains all information and methods for an image
+ * Contains all information and methods for an image.
  *
  * @author Adrian Schuetz
  */
 
-public class Picture
-		extends Canvas {
-	private BufferedImage image;
-	private BufferedImage scaledImage;
-	private double factor = 1.0;
-	private String imageName = "";
-	private double scalingFactor;
+public class Picture extends Canvas {
+    /** 8 bit shift. */
+    private static final int SHIFT8 = 8;
+    /** 16 bit shift. */
+    private static final int SHIFT16 = 16;
+    /** Number of components (3) in RGB color value. */
+    private static final int COLOR_VALUES = 3;
+    /**
+     * Number subtracted from slider value for exponent used to calculate
+     * scaling factor.
+     */
+    private static final int SCALING_FACTOR_POWER_ADJUSTMENT = 3;
+    /**
+     * Value to which rectangle's width or height is set if original value <= 0.
+     */
+    private static final int MIN_WIDTH_OR_HEIGHT = 10;
+    /** Hex value ff = 255 decimal. */
+    private static final int HEX_FF = 0xff;
+    /** Image. */
+    private BufferedImage image;
+    /** Scaled image. */
+    private BufferedImage scaledImage;
+    /** Magnification factor. */
+    private double factor = 1.0;
+    /** Name of image. */
+    private String imageName = "";
+    /** Scaling factor. */
+    private double scalingFactor;
 
-	/**
-	 * method: Picture
-	 * description: constructor
-	 *
-	 * @author Adrian Schuetz
-	 * @param file (image file)
-	 * @exception exception
-	 */
-	public Picture(final File file) throws IOException {
-		loadImage(file);
-		this.imageName = file.getName();
-	}
+    /**
+     * Constructor.
+     *
+     * @author Adrian Schuetz
+     * @param file (image file)
+     * @exception exception
+     */
+    public Picture(final File file) throws IOException {
+        loadImage(file);
+        this.imageName = file.getName();
+    }
 
-	/**
-	 * method: Picture
-	 * description: constructor
-	 *
-	 * @author Adrian Schuetz
-	 * @param image
-	 */
-	public Picture(final BufferedImage image) {
-		this.image = image;
-	}
+    /**
+     * Constructor.
+     *
+     * @author Adrian Schuetz
+     * @param bufferedImage
+     */
+    public Picture(final BufferedImage bufferedImage) {
+        this.image = bufferedImage;
+    }
 
-	/**
-	 * method: loadImage
-	 * description: loads an image file
-	 *
-	 * @author Adrian Schuetz
-	 * @param file (image file)
-	 * @exception exception
-	 */
-	private void loadImage(final File file) throws IOException {
-		this.image = ImageIO.read(file);
-		if (image.getType() == 0) {
-			// if type is custom do not load image
-			this.image = null;
-		}
-	}
+    /**
+     * Loads an image file.
+     *
+     * @author Adrian Schuetz
+     * @param file (image file)
+     * @exception exception
+     */
+    private void loadImage(final File file) throws IOException {
+        this.image = ImageIO.read(file);
 
-	/**
-	 * method: cutout
-	 * description: replace the image with a cutout of the original image
-	 *
-	 * @author Adrian Schuetz
-	 * @param cutout rectangle
-	 */
-	public void cutout(final Rectangle rectangle) {
-		// map rectangle coordinates from scaled image to original image
-		final int startX = java.lang.Math.round((float) (rectangle.x / scalingFactor));
-		final int startY = java.lang.Math.round((float) (rectangle.y / scalingFactor));
-		final int endX = java.lang.Math.round((float) ((rectangle.x + rectangle.width) / scalingFactor));
-		final int endY = java.lang.Math.round((float) ((rectangle.y + rectangle.height) / scalingFactor));
+        if (image.getType() == 0) {
+            // if type is custom do not load image
+            this.image = null;
+        }
+    }
 
-		// the cutout is processed from the scaled image
-		try {
-			this.image = this.image.getSubimage(startX, startY, (endX - startX), (endY - startY));
-		} catch (final Exception e) {
-			System.out.println("startX: " + startX);
-			System.out.println("startY: " + startY);
-			System.out.println("endX - startX: " + (endX - startX));
-			System.out.println("endy - startY: " + (endY - startY));
-		}
-	}
+    /**
+     * Replace the image with a cutout of the original image.
+     *
+     * @author Adrian Schuetz
+     * @param rectangle rectangle
+     */
+    public void cutout(final Rectangle rectangle) {
+        // map rectangle coordinates from scaled image to original image
+        final int startX = java.lang.Math
+                .round((float) (rectangle.x / scalingFactor));
+        final int startY = java.lang.Math
+                .round((float) (rectangle.y / scalingFactor));
+        final int endX = java.lang.Math.round(
+                (float) ((rectangle.x + rectangle.width) / scalingFactor));
+        final int endY = java.lang.Math.round(
+                (float) ((rectangle.y + rectangle.height) / scalingFactor));
 
-	/**
-	 * method: scaleImage
-	 * description: scales the image
-	 *
-	 * @author Adrian Schuetz
-	 * @param scaleFactor
-	 */
-	private void scaleImage(final double scaleFactor) {
-		final int width = image.getWidth();
-		final int height = image.getHeight();
-		final int scaleWidth = (int) (width * scaleFactor);
-		final int scaleHeight = (int) (height * scaleFactor);
-		this.scaledImage = new BufferedImage(scaleWidth, scaleHeight, image.getType());
-		// creates an graphics2d object, which is used to draw the scaled image
-		final Graphics2D g2 = scaledImage.createGraphics();
-		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-				RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
-		final AffineTransform at = AffineTransform.getScaleInstance(scaleFactor, scaleFactor);
-		g2.drawRenderedImage(image, at);
-	}
+        // the cutout is processed from the scaled image
+        try {
+            this.image = this.image.getSubimage(startX, startY, (endX - startX),
+                    (endY - startY));
+        } catch (final Exception e) {
+            System.out.println("startX: " + startX);
+            System.out.println("startY: " + startY);
+            System.out.println("endX - startX: " + (endX - startX));
+            System.out.println("endy - startY: " + (endY - startY));
+        }
+    }
 
-	/**
-	 * method: getImage
-	 * description: returns the image
-	 *
-	 * @author Adrian Schuetz
-	 * @return image
-	 */
-	public BufferedImage getImage() {
-		return image;
-	}
+    /**
+     * Scales the image.
+     *
+     * @author Adrian Schuetz
+     * @param scaleFactor
+     */
+    private void scaleImage(final double scaleFactor) {
+        final int width = image.getWidth();
+        final int height = image.getHeight();
+        final int scaleWidth = (int) (width * scaleFactor);
+        final int scaleHeight = (int) (height * scaleFactor);
+        this.scaledImage = new BufferedImage(scaleWidth, scaleHeight,
+                image.getType());
+        // creates an graphics2d object, which is used to draw the scaled image
+        final Graphics2D g2 = scaledImage.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+        final AffineTransform at = AffineTransform.getScaleInstance(scaleFactor,
+                scaleFactor);
+        g2.drawRenderedImage(image, at);
+    }
 
-	/**
-	 * method: getImageName
-	 * description: returns the image name
-	 *
-	 * @author Adrian Schuetz
-	 * @return image name
-	 */
-	public String getImageName() {
-		return this.imageName;
-	}
+    /**
+     * Returns the image.
+     *
+     * @author Adrian Schuetz
+     * @return image
+     */
+    public BufferedImage getImage() {
+        return image;
+    }
 
-	/**
-	 * method: computeScaleFactor
-	 * description: computes the scale factor from the user-defined rectangle
-	 *
-	 * @author Adrian Schuetz
-	 * @param width
-	 * @param height
-	 */
-	public void computeScaleFactor(final double width, final double height) {
-		final double imageWidth = (double) image.getWidth();
-		final double imageHeight = (double) image.getHeight();
-		double rectangleWidth;
-		double rectangleHeight;
-		if (width <= 0) {
-			rectangleWidth = 10;
-		} else {
-			rectangleWidth = width;
-		}
-		if (height <= 0) {
-			rectangleHeight = 10;
-		} else {
-			rectangleHeight = height;
-		}
-		// compute factor
-		if ((rectangleHeight / rectangleWidth) >= (imageHeight / imageWidth)) {
-			this.factor = rectangleWidth / imageWidth;
-		} else {
-			this.factor = rectangleHeight / imageHeight;
-		}
-	}
+    /**
+     * Returns the image name.
+     *
+     * @author Adrian Schuetz
+     * @return image name
+     */
+    public String getImageName() {
+        return this.imageName;
+    }
 
-	/**
-	 * method: getScaledImage
-	 * description: returns a scaled image to a given slider value
-	 *
-	 * @author Adrian Schuetz
-	 * @return scaled image
-	 */
-	public BufferedImage getScaledImage(final int sliderValue) {
-		this.scalingFactor = factor * Math.pow((Math.sqrt(2.0)), (sliderValue - 3));
-		scaleImage(scalingFactor);
-		return this.scaledImage;
-	}
+    /**
+     * Computes the scale factor from the user-defined rectangle.
+     *
+     * @author Adrian Schuetz
+     * @param width
+     * @param height
+     */
+    public void computeScaleFactor(final double width, final double height) {
+        final double imageWidth = (double) image.getWidth();
+        final double imageHeight = (double) image.getHeight();
+        double rectangleWidth;
+        double rectangleHeight;
 
-	/**
-	 * method: paint
-	 * description: overwrites the paint method (Canvas)
-	 *
-	 * @author Adrian Schuetz
-	 * @param g2d
-	 */
-	public void paint(final Graphics g2d) {
-		g2d.drawImage(image, 1, 1, this);
-	}
+        if (width <= 0) {
+            rectangleWidth = MIN_WIDTH_OR_HEIGHT;
+        } else {
+            rectangleWidth = width;
+        }
 
-	/**
-	 * method: getHeight
-	 * description: returns the image height
-	 *
-	 * @author Adrian Schuetz
-	 * @return image height
-	 */
-	public int getHeight() {
-		return image.getHeight(this);
-	}
+        if (height <= 0) {
+            rectangleHeight = MIN_WIDTH_OR_HEIGHT;
+        } else {
+            rectangleHeight = height;
+        }
 
-	/**
-	 * method: getWidth
-	 * description: returns the image width
-	 *
-	 * @author Adrian Schuetz
-	 * @return image width
-	 */
-	public int getWidth() {
-		return image.getWidth(this);
-	}
+        // compute factor
+        if ((rectangleHeight / rectangleWidth) >= (imageHeight / imageWidth)) {
+            this.factor = rectangleWidth / imageWidth;
+        } else {
+            this.factor = rectangleHeight / imageHeight;
+        }
+    }
 
-	/**
-	 * method: getPixelMatrix
-	 * description: returns a 3D pixel matrix (width, height, rgb)
-	 *
-	 * @author Adrian Schuetz
-	 * @return pixel matrix
-	 */
-	public int[][][] getPixelMatrix() {
-		final int width = image.getWidth(this), height = image.getHeight(this);
-		final int pixels[] = new int[height * width];
-		final int pixelMatrix[][][] = new int[height][width][3];
-		final PixelGrabber grabber = new PixelGrabber(image, 0, 0, width, height, pixels, 0, width);
-		try {
-			grabber.grabPixels();
-			for (int row = 0; row < height; row++) {
-				for (int column = 0; column < width; column++) {
-					final int pixel = pixels[row * width + column];
-					pixelMatrix[row][column][0] = (pixel >> 16) & 0xff; // red
-					pixelMatrix[row][column][1] = (pixel >> 8) & 0xff; // green
-					pixelMatrix[row][column][2] = (pixel) & 0xff; // blue
-				}
-			}
-		} catch (final InterruptedException e) {
-		}
-		return pixelMatrix;
-	}
+    /**
+     * Returns a scaled image to a given slider value.
+     *
+     * @author Adrian Schuetz
+     * @param sliderValue value of the scaling slider.
+     * @return scaled image
+     */
+    public BufferedImage getScaledImage(final int sliderValue) {
+        this.scalingFactor = factor * Math.pow((Math.sqrt(2.0)),
+                (sliderValue - SCALING_FACTOR_POWER_ADJUSTMENT));
+        scaleImage(scalingFactor);
+        return this.scaledImage;
+    }
+
+    /**
+     * Overwrites the paint method (Canvas).
+     *
+     * @author Adrian Schuetz
+     * @param g2d
+     */
+    public void paint(final Graphics g2d) {
+        g2d.drawImage(image, 1, 1, this);
+    }
+
+    /**
+     * Returns the image height.
+     *
+     * @author Adrian Schuetz
+     * @return image height
+     */
+    public int getHeight() {
+        return image.getHeight(this);
+    }
+
+    /**
+     * Returns the image width.
+     *
+     * @author Adrian Schuetz
+     * @return image width
+     */
+    public int getWidth() {
+        return image.getWidth(this);
+    }
+
+    /**
+     * Returns a 3D pixel matrix (width, height, rgb).
+     *
+     * @author Adrian Schuetz
+     * @return pixel matrix
+     */
+    public int[][][] getPixelMatrix() {
+        final int width = image.getWidth(this);
+        final int height = image.getHeight(this);
+        final int[] pixels = new int[height * width];
+        final int[][][] pixelMatrix = new int[height][width][COLOR_VALUES];
+        final PixelGrabber grabber = new PixelGrabber(image, 0, 0, width,
+                height, pixels, 0, width);
+        try {
+            grabber.grabPixels();
+
+            for (int row = 0; row < height; row++) {
+                for (int column = 0; column < width; column++) {
+                    final int pixel = pixels[row * width + column];
+                    pixelMatrix[row][column][0] = getRedComponent(pixel);
+                    pixelMatrix[row][column][1] = getGreenComponent(pixel);
+                    pixelMatrix[row][column][2] = getBlueComponent(pixel);
+                }
+            }
+        } catch (final InterruptedException e) {
+        }
+
+        return pixelMatrix;
+    }
+
+    private int getBlueComponent(final int pixel) {
+        return (pixel) & HEX_FF;
+    }
+
+    private int getGreenComponent(final int pixel) {
+        return (pixel >> SHIFT8) & HEX_FF;
+    }
+
+    private int getRedComponent(final int pixel) {
+        return (pixel >> SHIFT16) & HEX_FF;
+    }
 }
