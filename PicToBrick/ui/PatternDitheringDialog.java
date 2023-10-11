379cfdee6b09d88c9ff65bd609d7b1e692813835
jwatne <john.watne@gmail.com>
2023-10-11 04:04:19 -0500

    Checkstyle fixes.

--- PicToBrick/ui/PatternDitheringDialog.java
+++ PicToBrick/ui/PatternDitheringDialog.java
@@ -1,92 +1,109 @@
 package pictobrick.ui;
 
-import javax.swing.*;
 import javax.swing.border.TitledBorder;
+
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ResourceBundle;
+
 import javax.swing.BorderFactory;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
 
 /**
- * class:          PatternDitheringDialog
- * layer:          Gui (three tier architecture)
- * description:    dialog for luminance value distance
- * @author         Adrian Schuetz
+ * Dialog for luminance value distance.
+ *
+ * @author Adrian Schuetz
  */
-public class PatternDitheringDialog
-extends JDialog
-implements ActionListener
-{
-	private static ResourceBundle textbundle = ResourceBundle.getBundle("Resources.TextResource");
-	private JSlider distance;
+public class PatternDitheringDialog extends JDialog
+        implements ActionListener, PicToBrickDialog {
+    /** Major tick spacing for distance slider. */
+    private static final int MAJOR_TICK_SPACING = 10;
+    /** Default distance slider value. */
+    private static final int DEFAULT_DISTANCE = 75;
+    /** Maximum distance slider value. */
+    private static final int MAX_DISTANCE = 100;
+    /** Text resource bundle. */
+    private static ResourceBundle textbundle = ResourceBundle
+            .getBundle("Resources.TextResource");
+    /** Distance slider. */
+    private final JSlider distance;
 
-	/**
-	 * method:          PatternDitheringDialog
-	 * description:     constructor
-	 * @author          Adrian Schuetz
-	 * @param           owner
-	 */
-	public PatternDitheringDialog(Frame owner)
-	{
-		super(owner,textbundle.getString("dialog_patternDithering_frame"),true);
-		this.setLocation(100,100);
-		this.setResizable(false);
-		JPanel content = new JPanel(new BorderLayout());
-		JPanel textSlider = new JPanel(new BorderLayout());
-		JPanel text = new JPanel(new GridLayout(2,1,5,5));
-		JPanel slider = new JPanel(new GridLayout(1,1));
-		JPanel buttons = new JPanel();
-		//BUTTON
-		JButton ok = new JButton(textbundle.getString("button_ok"));
-		ok.setActionCommand("ok");
-		ok.addActionListener(this);
-		//LABEL
-		JLabel text1 = new JLabel(textbundle.getString("dialog_patternDithering_label_1"));
-		JLabel text2 = new JLabel(textbundle.getString("dialog_patternDithering_label_2"));
-		//Slider
-		distance = new JSlider(0,100,75);
-		distance.setMinorTickSpacing(1);
-		distance.setMajorTickSpacing(10);
-		distance.setPaintLabels(true);
-		distance.setPaintTicks(true);
-		distance.setSnapToTicks(true);
-		distance.setFocusable(false);
-		slider.add(distance);
-		text.add(text1);
-		text.add(text2);
-		textSlider.add(text, BorderLayout.NORTH);
-		textSlider.add(slider, BorderLayout.SOUTH);
-		TitledBorder textSliderBorder = BorderFactory.createTitledBorder(textbundle.getString("dialog_patternDithering_border"));
-		textSlider.setBorder(textSliderBorder);
-		textSliderBorder.setTitleColor(new Color(100,100,100));
-		buttons.add(ok);
-		content.add(textSlider, BorderLayout.NORTH);
-		content.add(buttons, BorderLayout.SOUTH);
-		this.getContentPane().add(content);
-		this.pack();
-		this.setVisible(true);
-	}
+    /**
+     * Constructor.
+     *
+     * @author Adrian Schuetz
+     * @param owner
+     */
+    public PatternDitheringDialog(final Frame owner) {
+        super(owner, textbundle.getString("dialog_patternDithering_frame"),
+                true);
+        this.setLocation(DEFAULT_PIXELS, DEFAULT_PIXELS);
+        this.setResizable(false);
+        final JPanel content = new JPanel(new BorderLayout());
+        final JPanel textSlider = new JPanel(new BorderLayout());
+        final JPanel text = new JPanel(new GridLayout(2, 1, 5, 5));
+        final JPanel slider = new JPanel(new GridLayout(1, 1));
+        final JPanel buttons = new JPanel();
+        // BUTTON
+        final JButton ok = new JButton(textbundle.getString("button_ok"));
+        ok.setActionCommand("ok");
+        ok.addActionListener(this);
+        // LABEL
+        final JLabel text1 = new JLabel(
+                textbundle.getString("dialog_patternDithering_label_1"));
+        final JLabel text2 = new JLabel(
+                textbundle.getString("dialog_patternDithering_label_2"));
+        // Slider
+        distance = new JSlider(0, MAX_DISTANCE, DEFAULT_DISTANCE);
+        distance.setMinorTickSpacing(1);
+        distance.setMajorTickSpacing(MAJOR_TICK_SPACING);
+        distance.setPaintLabels(true);
+        distance.setPaintTicks(true);
+        distance.setSnapToTicks(true);
+        distance.setFocusable(false);
+        slider.add(distance);
+        text.add(text1);
+        text.add(text2);
+        textSlider.add(text, BorderLayout.NORTH);
+        textSlider.add(slider, BorderLayout.SOUTH);
+        final TitledBorder textSliderBorder = BorderFactory.createTitledBorder(
+                textbundle.getString("dialog_patternDithering_border"));
+        textSlider.setBorder(textSliderBorder);
+        textSliderBorder.setTitleColor(GRANITE_GRAY);
+        buttons.add(ok);
+        content.add(textSlider, BorderLayout.NORTH);
+        content.add(buttons, BorderLayout.SOUTH);
+        this.getContentPane().add(content);
+        this.pack();
+        this.setVisible(true);
+    }
 
-	/**
-	 * method:          getDistance
-	 * description:     returns the maximum distance of the luminance values
-	 * @author          Adrian Schuetz
-	 * @return          distance
-	 */
-	public int getDistance(){
-		return distance.getValue();
-	}
+    /**
+     * Returns the maximum distance of the luminance values.
+     *
+     * @author Adrian Schuetz
+     * @return distance
+     */
+    public int getDistance() {
+        return distance.getValue();
+    }
 
-	/**
-	 * method:          actionPerformed
-	 * description:     ActionListener
-	 * @author          Adrian Schuetz
-	 * @param			event
-	 */
-	public void actionPerformed(ActionEvent event)
-	{
-		if (event.getActionCommand().contains("ok")){
-			this.setVisible(false);
-		}
-	}
+    /**
+     * ActionListener.
+     *
+     * @author Adrian Schuetz
+     * @param event
+     */
+    public void actionPerformed(final ActionEvent event) {
+        if (event.getActionCommand().contains("ok")) {
+            this.setVisible(false);
+        }
+    }
 }
