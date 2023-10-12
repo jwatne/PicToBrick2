76a9017855af32a6211f7b3ac01113f6d69b0a00
jwatne <john.watne@gmail.com>
2023-10-12 05:04:44 -0500

    Checkstyle fixes.

--- PicToBrick/ui/WorkingDirectoryDialog.java
+++ PicToBrick/ui/WorkingDirectoryDialog.java
@@ -1,93 +1,103 @@
 package pictobrick.ui;
 
-import javax.swing.*;
 import javax.swing.border.TitledBorder;
-import javax.swing.BorderFactory;
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
-import java.util.*;
 
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ResourceBundle;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
- * class:          WorkingDirectoryDialog
- * layer:          Gui (three tier architecture)
- * description:    dialog for choosing the working directory
- * @author         Adrian Schuetz
+ * Dialog for choosing the working directory.
+ *
+ * @author Adrian Schuetz
  */
-public class WorkingDirectoryDialog
-extends JDialog
-implements ActionListener
-{
-	private int buttonNumber = 0;
-	private static ResourceBundle textbundle = ResourceBundle.getBundle("Resources.TextResource");
+public class WorkingDirectoryDialog extends JDialog
+        implements ActionListener, PicToBrickDialog {
+    /** 1 for choosing a new directory; 0 otherwise. */
+    private int buttonNumber = 0;
+    /** Text resource bundle. */
+    private static ResourceBundle textbundle = ResourceBundle
+            .getBundle("Resources.TextResource");
 
-	/**
-	 * method:          WorkingDirectoryDialog
-	 * description:     constructor
-	 * @author          Adrian Schuetz
-	 * @param           owner
-	 * @param           working directory (old)
-	 */
-	public WorkingDirectoryDialog(Frame owner, File workingDirectoryOld)
-	{
-		super(owner, textbundle.getString("dialog_workingDirectory_frame") ,true);
-		this.setLocation(100,100);
-		this.setResizable(false);
-		JPanel content = new JPanel(new BorderLayout());
-		JPanel text = new JPanel();
-		JPanel buttons = new JPanel();
-		JLabel label = new JLabel();
-		if (workingDirectoryOld == null){
-			label.setText(textbundle.getString("dialog_workingDirectory_label"));
-		}else {
-			label.setText(workingDirectoryOld.getPath());
-		}
-		JButton newDirectory = new JButton(textbundle.getString("dialog_workingDirectory_button"));
-		newDirectory.setActionCommand("ok");
-		newDirectory.addActionListener(this);
-		JButton cancel = new JButton(textbundle.getString("button_cancel"));
-		cancel.setActionCommand("cancel");
-		cancel.addActionListener(this);
-		buttons.add(newDirectory);
-		buttons.add(cancel);
-		text.add(label);
-		TitledBorder textBorder = BorderFactory.createTitledBorder(textbundle.getString("dialog_workingDirectory_border"));
-		text.setBorder(textBorder);
-		textBorder.setTitleColor(new Color(100,100,100));
-		content.add(text, BorderLayout.CENTER);
-		content.add(buttons, BorderLayout.SOUTH);
-		this.getContentPane().add(content);
-		this.pack();
-		this.setVisible(true);
-	}
+    /**
+     * Constructor.
+     *
+     * @author Adrian Schuetz
+     * @param owner
+     * @param workingDirectoryOld
+     */
+    public WorkingDirectoryDialog(final Frame owner,
+            final File workingDirectoryOld) {
+        super(owner, textbundle.getString("dialog_workingDirectory_frame"),
+                true);
+        this.setLocation(DEFAULT_PIXELS, DEFAULT_PIXELS);
+        this.setResizable(false);
+        JPanel content = new JPanel(new BorderLayout());
+        JPanel text = new JPanel();
+        JPanel buttons = new JPanel();
+        JLabel label = new JLabel();
 
+        if (workingDirectoryOld == null) {
+            label.setText(
+                    textbundle.getString("dialog_workingDirectory_label"));
+        } else {
+            label.setText(workingDirectoryOld.getPath());
+        }
 
-	/**
-	 * method:          getButton
-	 * description:     returns the button number: 1 for choosing a new directory
-	 * @author          Adrian Schuetz
-	 * @return          1 for choosing a new directory, 0 else
-	 */
-	public int getButton(){
-		return this.buttonNumber;
-	}
+        JButton newDirectory = new JButton(
+                textbundle.getString("dialog_workingDirectory_button"));
+        newDirectory.setActionCommand("ok");
+        newDirectory.addActionListener(this);
+        JButton cancel = new JButton(textbundle.getString("button_cancel"));
+        cancel.setActionCommand("cancel");
+        cancel.addActionListener(this);
+        buttons.add(newDirectory);
+        buttons.add(cancel);
+        text.add(label);
+        TitledBorder textBorder = BorderFactory.createTitledBorder(
+                textbundle.getString("dialog_workingDirectory_border"));
+        text.setBorder(textBorder);
+        textBorder.setTitleColor(GRANITE_GRAY);
+        content.add(text, BorderLayout.CENTER);
+        content.add(buttons, BorderLayout.SOUTH);
+        this.getContentPane().add(content);
+        this.pack();
+        this.setVisible(true);
+    }
 
+    /**
+     * Returns the button number: 1 for choosing a new directory.
+     *
+     * @author Adrian Schuetz
+     * @return 1 for choosing a new directory, 0 else
+     */
+    public int getButton() {
+        return this.buttonNumber;
+    }
 
-	/**
-	 * method:          actionPerformed
-	 * description:     ActionListener
-	 * @author          Adrian Schuetz
-	 * @param			event
-	 */
-	public void actionPerformed(ActionEvent event)
-	{
-		if (event.getActionCommand().contains("ok")){
-			buttonNumber = 1;
-			this.setVisible(false);
-		}
-		if (event.getActionCommand().contains("cancel")){
-			this.setVisible(false);
-		}
-	}
+    /**
+     * ActionListener.
+     *
+     * @author Adrian Schuetz
+     * @param event
+     */
+    public void actionPerformed(final ActionEvent event) {
+        if (event.getActionCommand().contains("ok")) {
+            buttonNumber = 1;
+            this.setVisible(false);
+        }
+
+        if (event.getActionCommand().contains("cancel")) {
+            this.setVisible(false);
+        }
+    }
 }
