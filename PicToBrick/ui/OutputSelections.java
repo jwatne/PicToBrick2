3fbe2e91c42f61b328eacf5888cbca1dfb3aa38e
jwatne <john.watne@gmail.com>
2023-11-03 19:19:37 -0500

    Add param Javadoc comments.

--- PicToBrick/ui/OutputSelections.java
+++ PicToBrick/ui/OutputSelections.java
@@ -3,6 +3,12 @@ package pictobrick.ui;
 /**
  * Record containing output file selections. Initial coding by John Watne
  * 10/2023.
+ *
+ * @param graphic
+ * @param configuration
+ * @param material
+ * @param instruction
+ * @param xml
  */
 public record OutputSelections(boolean graphic, boolean configuration,
         boolean material, boolean instruction, boolean xml) {
